0b412b0589a5321f41ac6997b55a9c3facacb1ae
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-08-05 17:08:40 +0530

    wrap for user

--- src/component/RecipeCard.jsx
+++ src/component/RecipeCard.jsx
@@ -1,11 +1,11 @@
 export const RecipeCard = ({ menu, restaurantName }) => {
   
   return (
-    <div className="d-flex gap-4 mb-5  card-center">
+    <div className="d-flex flex-wrap gap-4 mb-5  card-center">
       {menu.map((menuItem, index) => {
         const { name, imgSrc, price , qty} = menuItem;
         return (
-          <div className="card text-start" style={{ width: "18rem" }} key={index}>
+          <div className="card text-start " style={{ width: "18rem" }} key={index}>
             <img src={imgSrc} className="card-img-top" alt={name} />
             <div className="card-body">
               <h5 className="card-title">{name}</h5>
